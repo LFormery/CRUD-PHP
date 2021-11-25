0802d4982015558ee54d9b9f5bd50defc880f6b7
LFormery <formery.louise@gmail.com>
2021-11-25 22:19:40 +0100

    Solve new User in recup.php

--- recup.php
+++ recup.php
@@ -1,19 +1,16 @@
 <?php
-require "class.php";
+require "class.php"; 
   if(isset($_POST['valider']))
   {
-  	if(isset($_POST['use-name']) && isset($_POST['user-prenom']) && isset($_POST['user-email']))
+  	if(isset($_POST['user-name']) && isset($_POST['user-prenom']) && isset($_POST['user-email']))
   	{
-  		if(!empty($_POST['user-name']) AND !empty($_POST['user-prenom']) AND !empty($_POST['user-email']))
-  		{
            $name=htmlspecialchars($_POST['user-name']);
            $prenom=htmlspecialchars($_POST['user-prenom']);
            $email=htmlspecialchars($_POST['user-email']);
-
+           //echo   $name , $prenom , $email ;
              
-           // $User2= new User ( $name , $prenom ,   $email );
-          echo   $name , $prenom ,   $email ;
-  		}
+          $user2 = new User ($name, $prenom, $email);
+          var_dump($user2);	
   	}
   }
 
